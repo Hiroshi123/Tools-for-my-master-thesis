feed08b7df05b3ebbb07579946e413cd53af89bc
hiroshi <vrodxda@gmail.com>
2016-08-13 20:09:57 +0200

    some updates

--- MyLib/myComplex.hs
+++ MyLib/myComplex.hs
@@ -6,10 +6,12 @@ module MyLib.MyComplex
     Complex'(..)
   ) where
 
+import Control.Monad
 import MyLib.Matrix
 import MyLib.General
 
 data Complex' = Complex' (Double,Double) deriving (Show)
+
 
 class MyComplex where
   
@@ -18,20 +20,17 @@ class MyComplex where
   iSub :: Complex' -> Complex' -> Complex'
   iMul :: Complex' -> Complex' -> Complex'
   iDiv :: Complex' -> Complex' -> Complex'
-
-  iDot :: [[Complex']] -> [[Complex']] -> [[Complex']]
   
+  iDot :: [[Complex']] -> [[Complex']] -> [[Complex']]
   
   --change real to complex
   toComplex  :: Double -> Complex'
-
+  
   --change real to complex
   toReal  :: Complex' -> Double
   
-  
   --get conjuguate
   conjuguate :: Complex' -> Complex'
-
   
   
 instance MyComplex where
@@ -46,15 +45,20 @@ instance MyComplex where
   iDot a b = group (length a) ( map ( foldl (iAdd) (Complex' (0,0)) ) [ (map2 (iMul) x y)  | x <- a, y <- t])
      where t = t' b
            
-           
+                   
            
   --change real to complex
+  --toComplex (Left (Complex' (a,b))) = Complex' (a,b)
+  --toComplex (Complex' (a,b))) = Complex' (a,0)
+  
+  
   toComplex a = Complex' (a,0)
-
+  
+  
   --change complex to real( just discard imaginary part)
   toReal (Complex'(a,b)) = a
   
   --take conjuguate
   conjuguate (Complex'(a,b)) = Complex' (a,-b)
-
+  
   
